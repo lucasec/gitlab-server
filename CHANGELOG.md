17e3c4b3a48765763cdfd571abe2ec5e418cddde
Lucas Christian <lucas@lucasec.com>
2013-06-24 08:39:29 -0500

    Add metadata.json, move to version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of gitlab-server.
 
+## 1.0.0:
+
+* First release on Opscode site
+
 ## 0.1.0:
 
 * Initial release of gitlab-server
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'gitlab-server'
 maintainer       'Lucas Christian'
 maintainer_email 'lucas@lucasec.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures a Gitlab server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 %w{ redisio openssl rvm python git build-essential database mysql nginx yum }.each do |dependency|
   depends dependency
 end
